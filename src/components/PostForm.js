a071f99991dc0bd567d69035f1eaa06875dfa687
Noela-Murugi <murugin254@gmail.com>
2022-11-09 00:56:17 +0300

    add function form and usestates

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -3,4 +3,14 @@ import Axios from "axios";
 import "../Form.css";
 import { useNavigate } from "react-router-dom";
 
+function Form({ setData, data }) {
+  const [jobTitle, setTitle] = useState("");
+  const [salary, setSalary] = useState("");
+  const [description, setDescription] = useState("");
+  const [date, setDeadline] = useState("");
+  const [location, setLocation] = useState("");
+  const [requirements, setRequirements] = useState("");
+  const navigate = useNavigate();
 
+ 
+export default Form;
