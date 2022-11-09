1ab24a0b33b56ceea1e6ed0ae7c8de529a6f1658
Noela-Murugi <murugin254@gmail.com>
2022-11-09 04:30:22 +0300

    sample data

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,7 +1,19 @@
 
 
 export const jobs = {
-    
+    "lists": [
+       {
+            "id": 1,
+            "image": "https://cdn-icons-png.flaticon.com/512/2936/2936630.png",
+            "title": "Cyber Security",
+            "location": "Nairobi",
+            "deadline": "17/09/2022",
+            "requirements": "University graduate",
+            "description": "Is the protection of computer systems from information disclosure, as well as from the disruption or misdirection of the services they provide",
+            "salary": 190000,
+        },
+
+    ]
   }
 
 export default jobs
