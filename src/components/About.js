2e2e08d7879a5b37100c49f7e7ae173a17240e51
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2022-11-09 20:30:26 +0300

    almost done with about component

--- src/components/About.js
+++ src/components/About.js
@@ -15,6 +15,22 @@ function About() {
           </p>
         </div>
       </div>
+      <div className="row">
+        <h2>Our Values</h2>
+        <strong>MISSION:</strong> <br></br> Our mission is to make job hunting
+        easy for everyone, <br /> connecting YOU with millions of potential
+        employers around the globe.
+        <br /> Relax you don't have to dread the process anymore!!
+        <br />
+        <p>
+          {" "}
+          <strong>VISION:</strong> <br></br> Our vision at Job Finder is to help
+          make job hunting as effortless <br />
+          and as smooth as possible for those just starting out and also to
+          experienced veterans.
+          <br /> Find a job, at the comfort of your home.
+        </p>
+      </div>
     </>
   );
 }
