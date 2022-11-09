49a28fd4689fd8b8369806ebff5eca6c96fe057a
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2022-11-09 12:05:49 +0300

    creating h3

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,7 +6,13 @@ function Landing() {
   <h1>
     Find A <span className="anything">Job</span> That <br></br>
     <span className="anything"> Matches</span> <br></br> Your Passion
-  </h1>);
+  </h1>
+  <h3>
+        Hand-picked opportunities to work from home, remotely, freelance,
+        <br></br>
+        full-time, part-time, contract and internships.
+      </h3>
+      );
 }
 
 export default Landing;
