5b2f62073203e1d0e6b3b012a13969e36519aa86
Noela-Murugi <murugin254@gmail.com>
2022-11-09 04:34:46 +0300

    updating consts

--- src/components/CategoriesList.js
+++ src/components/CategoriesList.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
+const CategoriesItem = ({ jobs }) => {
 
+  const params = useParams()
+  console.log(params)
+  console.log(jobs)
+
+  
+}
 
 export default CategoriesItem
 
