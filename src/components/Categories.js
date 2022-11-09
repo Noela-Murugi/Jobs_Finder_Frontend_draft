f2f833e85787dde9ad4ca4439e56eca166fb15f3
Noela-Murugi <murugin254@gmail.com>
2022-11-09 04:50:16 +0300

    update const and usestates

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -4,6 +4,14 @@ import { Link} from 'react-router-dom'
 
 import jobs from './Jobs.js'
 
+const Categories = () => {
 
+  const [search, setSearch] = useState('')
+
+  const filteredList =
+  jobs.lists.filter(list => list.title.toLowerCase().includes(search.toLowerCase()))
+
+  
+}
 
 export default Categories
