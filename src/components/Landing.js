bcb67e61d9896c5c79d14c7c854dbe56d0497200
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2022-11-09 12:07:01 +0300

    inserting an image

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,17 +2,19 @@ import React from "react";
 
 function Landing() {
   return (
-  <div id="landing" className="container-fluid text-center bg-grey">
-  <h1>
-    Find A <span className="anything">Job</span> That <br></br>
-    <span className="anything"> Matches</span> <br></br> Your Passion
-  </h1>
-  <h3>
+    <div id="landing" className="container-fluid text-center bg-grey">
+      <h1>
+        Find A <span className="anything">Job</span> That <br></br>
+        <span className="anything"> Matches</span> <br></br> Your Passion
+      </h1>
+      <h3>
         Hand-picked opportunities to work from home, remotely, freelance,
         <br></br>
         full-time, part-time, contract and internships.
       </h3>
-      );
+      <img src="https://media.istockphoto.com/id/1365606637/photo/shot-of-a-young-businesswoman-using-a-digital-tablet-while-at-work.jpg?b=1&s=170667a&w=0&k=20&c=iQ8qVJ5m8VrC_CwdNlzqAwKVE4r7SOdV2UZoWrGcmSQ=" />
+    </div>
+  );
 }
 
 export default Landing;
