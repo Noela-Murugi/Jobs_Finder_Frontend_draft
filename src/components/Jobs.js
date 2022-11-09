a5ddb614214280b9cba9d45af80865ed55f71627
Noela-Murugi <murugin254@gmail.com>
2022-11-09 04:32:33 +0300

    sample data3

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -12,7 +12,17 @@ export const jobs = {
             "description": "Is the protection of computer systems from information disclosure, as well as from the disruption or misdirection of the services they provide",
             "salary": 190000,
         },
-
+        {
+            "id": 2,
+            "image": "https://cdn-icons-png.flaticon.com/512/2936/2936630.png",
+            "title": "Web Development",
+            "location": "Nakuru",
+            "deadline": "19/09/2022",
+            "requirements": "Secondary graduate",
+            "description": "Is the work involved in developing a website for the Internet or an intranet.",
+            "salary": 90500,
+        },
+ 
     ]
   }
 
