01db2ed9844ddc992a4c9b8b14cfacb972455726
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2022-11-09 12:04:42 +0300

    adding h1

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,1 +1,12 @@
 import React from "react";
+
+function Landing() {
+  return (
+  <div id="landing" className="container-fluid text-center bg-grey">
+  <h1>
+    Find A <span className="anything">Job</span> That <br></br>
+    <span className="anything"> Matches</span> <br></br> Your Passion
+  </h1>);
+}
+
+export default Landing;
