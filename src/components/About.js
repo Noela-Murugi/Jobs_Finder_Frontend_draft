d0e1f62be493af0a5ad3beeed5b8f33a962bf541
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2022-11-09 20:26:54 +0300

    creating h1

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,21 @@
 import React from "react";
 
 function About() {
-  return ();
+  return (
+    <>
+      <div id="about" className="container-fluid">
+        <div className="row">
+          <h2>About us</h2>
+          <p>
+            Ever had a tough time finding your next job? Well, look no further,
+            <br />
+            Job Finder is here to make your job hunting way simpler.
+            <br /> Say goodbye to having to stress yourself, we've got you
+            sorted.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 export default About;
