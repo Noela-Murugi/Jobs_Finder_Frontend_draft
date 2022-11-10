64578c1454e710ef87563c69d6995380e664acc5
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2022-11-10 06:22:06 +0300

    fixing errors in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,5 @@
-import "../Footer.css";
 function Footer() {
   return (
-    <footer className="container-fluid text-center">
-      <a href="#myPage" title="To Top">
-        <span className="glyphicon glyphicon-chevron-up"></span>
-      </a>
-      <p> © 2022 Jobs Finder. Made By : Group 1</p>
-    </footer>
-  );
+    
+  )
 }
-export default Footer;
