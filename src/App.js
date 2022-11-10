84b03d55ffbe142e5aa65064c0cce9328afa649c
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2022-11-10 06:25:26 +0300

    fixing some minor linking issues

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
 import "./App.css";
-import Footer from "./components/Footer";
 function App() {
-  return (
-    <div className="App">
-      <Footer />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
