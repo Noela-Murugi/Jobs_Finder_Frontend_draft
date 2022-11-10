363e9d42e9eaf30f912cc3640b027b2324dd96c9
Ali Mukhtar <ali.mukhtar@student.moringaschool.com>
2022-11-10 06:53:43 +0300

    fixing some issues in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,9 @@
-import "../Navbar.css";
 function Navbar() {
   return (
     <nav class="navbar">
-      <div class="max-width">
-        <div class="logo">
-          <a href="#">Job Finder!</a>
-        </div>
-        <ul class="menu">
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#find jobs">Find jobs</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-        </ul>
+      <div class="logo">
+        <a href="#">Job Finder!</a>
       </div>
     </nav>
   );
 }
-export default Navbar;
